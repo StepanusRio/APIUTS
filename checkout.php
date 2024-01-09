a7257123004bbfa06cf65ceee1c00ca77bd76bd0
StepanusRio <stepanusriodefaardian@gmail.com>
2024-01-09 13:08:11 +0700

    Add : Checkout Fitur

---
+++ checkout.php
@@ -0,0 +1,49 @@
+<?php
+include "./connection.php";
+header("content-type:application/json");
+
+$idOrder = $_POST['idOrder'];
+$Alamat = $_POST['Alamat'];
+$Kota = $_POST['Kota'];
+$Provinsi = $_POST['Provinsi'];
+$Subtotal = $_POST['Subtotal'];
+$Ongkir = $_POST['Ongkir'];
+$TotalHarga = $_POST['TotalHarga'];
+$PaymentMethod = $_POST['PaymentMethod'];
+$TanggalCheckout = $_POST['TanggalCheckout'];
+$StatusPayment = $_POST['StatusPayment'];
+$username = $_POST['username'];
+$sql = "INSERT INTO tb_pro_checkout (
+idOrder,
+username,
+Alamat,Kota,
+Provinsi,
+Subtotal,
+Ongkir,
+TotalHarga,
+PaymentMethod,
+TanggalCheckout,
+StatusPayment) VALUES(
+'$idOrder',
+'$username',
+'$Alamat',
+'$Kota',
+'$Provinsi',
+$Subtotal,
+$Ongkir,
+$TotalHarga,
+'$PaymentMethod'
+,'$TanggalCheckout'
+,'$StatusPayment')";
+
+$hasil = mysqli_query($conn, $sql);
+
+if ($hasil) {
+  $getresult = 1;
+  $message = "Berhasil Checkout";
+} else {
+  $getresult = 1;
+  $message = "Gagal Checkout";
+}
+
+echo json_encode(array("result" => $getresult, "message" => $message));
